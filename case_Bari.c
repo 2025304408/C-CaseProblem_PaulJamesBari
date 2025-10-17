91505b67ae53ddac84b4ae58c36777b05a788994
2025304408 <pauljamesbari372@gmail.com>
2025-10-18 05:55:57 +0800

    Uploaded C case problem and output screenshot

---
+++ case_Bari.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // Display student information
+    printf("Name: Paul James Bari\n");
+    printf("ID Number: 2025304408\n");
+    printf("------------------------------\n");
+
+    // Variables for computation
+    int num1 = 10;
+    int num2 = 20;
+    int sum = num1 + num2;
+
+    // Display result
+    printf("Result: The sum of %d and %d is %d\n", num1, num2, sum);
+
+    return 0;
+}
